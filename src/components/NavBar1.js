02980aca9e9985bd2e0825df7b30fe55fab316f1
Lizeth Paola Delgadillo <lizdelga001@gmail.com>
2022-03-31 17:06:15 -0500

    responsive

--- src/components/NavBar1.js
+++ src/components/NavBar1.js
@@ -1,27 +1,28 @@
 import React from "react";
 import Nav from "react-bootstrap/Nav";
+import '../styles/NavBar1.css'
 
 function NavBar1() {
   return (
-    <div>
+    <div className="Nav">
       <Nav
         activeKey="/home"
         onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
       >
         <Nav.Item>
-          <Nav.Link href="/home">All Sesions</Nav.Link>
+          <Nav.Link >All Sesions</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href="/home">2022-06-06</Nav.Link>
+          <Nav.Link >2022-06-06</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href="/home">2022-09-02</Nav.Link>
+          <Nav.Link >2022-09-02</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href="/home">2023-03-19</Nav.Link>
+          <Nav.Link >2023-03-19</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href="/home">2022-06-08</Nav.Link>
+          <Nav.Link >2022-06-08</Nav.Link>
         </Nav.Item>
       </Nav>
     </div>
