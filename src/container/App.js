bbcb8799b2a11cd24a73775111fb33fb90060480
Lizeth Paola Delgadillo <lizdelga001@gmail.com>
2022-03-31 15:06:44 -0500

    Mio

--- src/container/App.js
+++ src/container/App.js
@@ -1,18 +1,10 @@
-<<<<<<< HEAD
 import Cards from "../components/Cards";
-=======
-import SideBar from "../components/SideBar";
->>>>>>> dev-daniel
 
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Cards/>
-=======
-      <SideBar/>
->>>>>>> dev-daniel
     </div>
   );
 }
