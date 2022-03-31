2e545acd4b9cf3bdf343980eb50a936805123427
Daniel3132 <danielcorrea3131@gmail.com>
2022-03-31 15:35:25 -0500

    categorias

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -4,32 +4,214 @@ import '../styles/sidebar.css'
 
 const SideBar = () => {
 
+    const handleOnchange = e => {
+        console.log(e.target.value)
+    }
+
     return (
         <div className='sidebarCont'>
-            <h1>Filters</h1>
-            <Accordion>
+            <h3>Filters</h3>
+            <div>
+                <input className='inputSearch' placeholder='Search' name='busqueda' />
+            </div>
+            <Accordion className="Accordionxd">
                 <Accordion.Item eventKey="0">
                     <Accordion.Header>Industry Segment</Accordion.Header>
-                    <Accordion.Body className="Accordionxd">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.
+                    <Accordion.Body >
+                        <div>
+                            <label>Division</label>
+                            <input
+                                onChange={handleOnchange}
+                                value='Division'
+                                type='radio'
+                                label='label'
+                                name='radio'
+                            />
+                        </div>
+                        <div>
+                            <label>Group</label>
+                            <input
+                                onChange={handleOnchange}
+                                value='Group'
+                                type='radio'
+                                label='label'
+                                name='radio'
+                            />
+                        </div>
+                        <div>
+                            <label>Markets</label>
+                            <input
+                                onChange={handleOnchange}
+                                value='Markets'
+                                type='radio'
+                                label='label'
+                                name='radio'
+                            />
+                        </div>
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="1">
-                    <Accordion.Header>Accordion Item #2</Accordion.Header>
+                    <Accordion.Header>Session Type</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.
+
+                        <div>
+                            <label>Strategist</label>
+                            <input
+                                onChange={handleOnchange}
+                                value='Strategist'
+                                type='radio'
+                                label='label'
+                                name='radio'
+                            />
+                        </div>
+
+
+                        <div>
+                            <label>Coordinator</label>
+                            <input
+                                onChange={handleOnchange}
+                                value='Coordinator'
+                                type='radio'
+                                label='label'
+                                name='radio'
+                            />
+                        </div>
+
+                        <div>
+                            <label>Liaison</label>
+                            <input
+                                onChange={handleOnchange}
+                                value='Liaison'
+                                type='radio'
+                                label='label'
+                                name='radio'
+                            />
+                        </div>
+
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="2">
+                    <Accordion.Header>Audience Type</Accordion.Header>
+                    <Accordion.Body>
+
+                        <div>
+                            <label>Trans*Female</label>
+                            <input
+                                onChange={handleOnchange}
+                                value='Trans'
+                                type='radio'
+                                label='label'
+                                name='radio'
+                            />
+                        </div>
+
+
+                        <div>
+                            <label>Man</label>
+                            <input
+                                onChange={handleOnchange}
+                                value='</'
+                                type='radio'
+                                label='label'
+                                name='radio'
+                            />
+                        </div>
+
+                        <div>
+                            <label>Female</label>
+                            <input
+                                onChange={handleOnchange}
+                                value='Female'
+                                type='radio'
+                                label='label'
+                                name='radio'
+                            />
+                        </div>
+
+                        <div>
+                            <label>Transexual Man</label>
+                            <input
+                                onChange={handleOnchange}
+                                value='Transexual'
+                                type='radio'
+                                label='label'
+                                name='radio'
+                            />
+                        </div>
+
+                        <div>
+                            <label>Hermaphrodite</label>
+                            <input
+                                onChange={handleOnchange}
+                                value='Hermaphrodite'
+                                type='radio'
+                                label='label'
+                                name='radio'
+                            />
+                        </div>
+
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="3">
+                    <Accordion.Header>Primary Topic</Accordion.Header>
+                    <Accordion.Body>
+
+                        <div>
+                            <label>Music</label>
+                            <input
+                                onChange={handleOnchange}
+                                value='Music'
+                                type='radio'
+                                label='label'
+                                name='radio'
+                            />
+                        </div>
+
+
+                        <div>
+                            <label>Toys</label>
+                            <input
+                                onChange={handleOnchange}
+                                value='Toys'
+                                type='radio'
+                                label='label'
+                                name='radio'
+                            />
+                        </div>
+
+                        <div>
+                            <label>Garden</label>
+                            <input
+                                onChange={handleOnchange}
+                                value='Garden'
+                                type='radio'
+                                label='label'
+                                name='radio'
+                            />
+                        </div>
+
+                        <div>
+                            <label>Jewelery</label>
+                            <input
+                                onChange={handleOnchange}
+                                value='Jewelery'
+                                type='radio'
+                                label='label'
+                                name='radio'
+                            />
+                        </div>
+
+                        <div>
+                            <label>Hermaphrodite</label>
+                            <input
+                                onChange={handleOnchange}
+                                value='Hermaphrodite'
+                                type='radio'
+                                label='label'
+                                name='radio'
+                            />
+                        </div>
+
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
