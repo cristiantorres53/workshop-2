098783cd1d60dd10a07ac5b0aa35192f189229df
cristiantorres53 <cristiancamilotorresrodriguez@gmail.com>
2022-03-31 15:20:38 -0500

    creacion de navbar

--- src/container/App.js
+++ src/container/App.js
@@ -1,9 +1,11 @@
+import React from "react";
+import NavBar1 from "../components/NavBar1";
 
 
 function App() {
   return (
     <div className="App">
-      Soy App
+      <NavBar1/>      
     </div>
   );
 }
